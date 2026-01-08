08a23b145370fc801daf3b6a6e90821c13dffd2d
Sudhanshu Choudhary <surgeonofdeath4405@gmail.com>
2026-01-08 21:56:32 +0530

    Keyword map implemented for lexer

---
+++ packages/core/src/lexer/keywords.ts
@@ -0,0 +1,25 @@
+//The lexer uses this map to distinguish identifiers from keywords.
+import { TokenType } from "./token";
+
+export const KEYWORDS: Record<string, TokenType> = {
+  int: TokenType.INT,
+  float: TokenType.FLOAT,
+  string: TokenType.STRING,
+  bool: TokenType.BOOL,
+
+  if: TokenType.IF,
+  else: TokenType.ELSE,
+  while: TokenType.WHILE,
+  stop: TokenType.STOP,
+
+  func: TokenType.FUNC,
+  return: TokenType.RETURN,
+
+  true: TokenType.TRUE,
+  false: TokenType.FALSE,
+  null: TokenType.NULL,
+
+  and: TokenType.AND,
+  or: TokenType.OR,
+  not: TokenType.NOT
+};
