c6cf23dd76fe2e26c0372183c5f101bd98bb3ad6
Sudhanshu Choudhary <surgeonofdeath4405@gmail.com>
2026-01-09 16:06:39 +0530

    Precendence order done!!!

---
+++ packages/core/src/parser/precedence.ts
@@ -0,0 +1,20 @@
+import { TokenType } from "../lexer/token";
+
+export const PRECEDENCE: Partial<Record<TokenType, number>> = {
+  [TokenType.OR]: 1,
+  [TokenType.AND]: 2,
+
+  [TokenType.EQUAL_EQUAL]: 3,
+  [TokenType.BANG_EQUAL]: 3,
+
+  [TokenType.GREATER]: 4,
+  [TokenType.GREATER_EQUAL]: 4,
+  [TokenType.LESS]: 4,
+  [TokenType.LESS_EQUAL]: 4,
+
+  [TokenType.PLUS]: 5,
+  [TokenType.MINUS]: 5,
+
+  [TokenType.STAR]: 6,
+  [TokenType.SLASH]: 6
+};
