c72e90f36dac74edf53aad692f92702b6b76a5b8
Sudhanshu Choudhary <surgeonofdeath4405@gmail.com>
2026-01-14 12:01:27 +0530

    add runtime value definitions

---
+++ packages/core/src/interpreter/values.ts
@@ -0,0 +1,60 @@
+export type RuntimeValue = | IntValue | FloatValue | StringValue | BoolValue | NullValue | FunctionValue;
+
+export interface IntValue {
+  type: "int";
+  value: number;
+}
+
+export interface FloatValue {
+  type: "float";
+  value: number;
+}
+
+export interface StringValue {
+  type: "string";
+  value: string;
+}
+
+export interface BoolValue {
+  type: "bool";
+  value: boolean;
+}
+
+export interface NullValue {
+  type: "null";
+}
+
+export interface FunctionValue {
+  type: "function";
+  name: string;
+  parameters: {
+    typeName: string;
+    name: string;
+  }[];
+  body: any;
+  closure: any; 
+}
+
+export const INT = (value: number): IntValue => ({
+  type: "int",
+  value,
+});
+
+export const FLOAT = (value: number): FloatValue => ({
+  type: "float",
+  value,
+});
+
+export const STRING = (value: string): StringValue => ({
+  type: "string",
+  value,
+});
+
+export const BOOL = (value: boolean): BoolValue => ({
+  type: "bool",
+  value,
+});
+
+export const NULL: NullValue = {
+  type: "null",
+};
