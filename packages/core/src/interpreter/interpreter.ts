4be1284ea498a3db92b00dfd12d921600dcdc08e
Sudhanshu Choudhary <surgeonofdeath4405@gmail.com>
2026-01-15 19:23:18 +0530

    program runner and execution entry point implemented!! Interpreter done

---
+++ packages/core/src/interpreter/interpreter.ts
@@ -0,0 +1,37 @@
+import { Statement } from "../ast/nodes";
+import { Environment } from "./environment";
+import { execute } from "./statements";
+import { ReturnSignal, StopSignal } from "./control";
+import { RuntimeValue, NULL } from "./values";
+
+export class Interpreter {
+  private globalEnv: Environment;
+
+  constructor() {
+    this.globalEnv = new Environment();
+  }
+
+  run(program: Statement[]): RuntimeValue {
+    try {
+      for (const stmt of program) {
+        execute(stmt, this.globalEnv);
+      }
+    } catch (signal) {
+      if (signal instanceof ReturnSignal) {
+        return signal.value;
+      }
+
+      if (signal instanceof StopSignal) {
+        throw new Error("Illegal 'stop' outside of loop");
+      }
+
+      throw signal;
+    }
+
+    return NULL;
+  }
+  
+  getGlobalEnvironment(): Environment {
+    return this.globalEnv;
+  }
+}
